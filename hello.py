2a0e78ba7c913be0b026b248c06c3942640aadd4
S <s35717@outlook.com>
2023-05-20 20:48:12 +0000

    `hello.py` asks user for name, and greets them
    Script asks user for their name. It checks whether it is a valid name -
    in this case, it must consist of purely alphabetic characters. Else, it
    will change the meesage to try again.
    
    Closes #1

--- hello.py
+++ hello.py
@@ -1,3 +1,15 @@
-word = "Hello"
-for i in word:
-    print(i)
+# Default value of `asked_before` is False
+asked_before = False
+while True:
+    name = ""
+    # Changes the question prompt if this is the first time or if it has been asked before
+    if asked_before == False:
+        name = input("What is your name?\n")
+    else:
+        name = input("Sorry. Can you write that again please?\n")
+    # Checks for a valid name - i.e. consists of only alphabetic characters
+    if name.isalpha():
+        print(f"Hello, {name}!")
+        asked_before = False
+    else:
+        asked_before = True
