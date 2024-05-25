d50d3ead98722ac29a53ded14d99535c84437e74
Maou25 <darylnicolas25@gmail.com>
2024-05-25 09:44:09 +0800

    updated

---
+++ vendor/laravel/framework/src/Illuminate/Console/View/Components/Ask.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Illuminate\Console\View\Components;
+
+use Symfony\Component\Console\Question\Question;
+
+class Ask extends Component
+{
+    /**
+     * Renders the component using the given arguments.
+     *
+     * @param  string  $question
+     * @param  string  $default
+     * @return mixed
+     */
+    public function render($question, $default = null, $multiline = false)
+    {
+        return $this->usingQuestionHelper(
+            fn () => $this->output->askQuestion(
+                (new Question($question, $default))
+                    ->setMultiline($multiline)
+            )
+        );
+    }
+}
